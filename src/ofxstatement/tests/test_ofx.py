be78e6e41e3730ece384616ff88beebb88317825
Andrey Lebedev <andrey@lebedev.lt>
2017-12-26 23:58:54 +0200

    Replace doctests with unit tests

--- src/ofxstatement/tests/test_ofx.py
+++ src/ofxstatement/tests/test_ofx.py
@@ -1,5 +1,4 @@
-import sys
-import doctest
+from unittest import TestCase
 import xml.dom.minidom
 
 from datetime import datetime
@@ -7,110 +6,100 @@ from datetime import datetime
 from ofxstatement.statement import Statement, StatementLine, BankAccount
 from ofxstatement import ofx
 
+SIMPLE_OFX = """<?xml version="1.0" ?>
+<!--
+OFXHEADER:100
+DATA:OFXSGML
+VERSION:102
+SECURITY:NONE
+ENCODING:UTF-8
+CHARSET:NONE
+COMPRESSION:NONE
+OLDFILEUID:NONE
+NEWFILEUID:NONE
+-->
+<OFX>
+    <SIGNONMSGSRSV1>
+        <SONRS>
+            <STATUS>
+                <CODE>0</CODE>
+                <SEVERITY>INFO</SEVERITY>
+            </STATUS>
+            <DTSERVER>20120303000000</DTSERVER>
+            <LANGUAGE>ENG</LANGUAGE>
+        </SONRS>
+    </SIGNONMSGSRSV1>
+    <BANKMSGSRSV1>
+        <STMTTRNRS>
+            <TRNUID>0</TRNUID>
+            <STATUS>
+                <CODE>0</CODE>
+                <SEVERITY>INFO</SEVERITY>
+            </STATUS>
+            <STMTRS>
+                <CURDEF>LTL</CURDEF>
+                <BANKACCTFROM>
+                    <BANKID>BID</BANKID>
+                    <ACCTID>ACCID</ACCTID>
+                    <ACCTTYPE>CHECKING</ACCTTYPE>
+                </BANKACCTFROM>
+                <BANKTRANLIST>
+                    <DTSTART/>
+                    <DTEND/>
+                    <STMTTRN>
+                        <TRNTYPE>CHECK</TRNTYPE>
+                        <DTPOSTED>20120212</DTPOSTED>
+                        <TRNAMT>15.40</TRNAMT>
+                        <FITID>1</FITID>
+                        <MEMO>Sample 1</MEMO>
+                    </STMTTRN>
+                    <STMTTRN>
+                        <TRNTYPE>CHECK</TRNTYPE>
+                        <DTPOSTED>20120212</DTPOSTED>
+                        <TRNAMT>25.00</TRNAMT>
+                        <FITID>2</FITID>
+                        <MEMO>Sample 2</MEMO>
+                        <BANKACCTTO>
+                            <BANKID>SNORAS</BANKID>
+                            <BRANCHID>VNO</BRANCHID>
+                            <ACCTID>LT1232</ACCTID>
+                            <ACCTTYPE>CHECKING</ACCTTYPE>
+                        </BANKACCTTO>
+                    </STMTTRN>
+                </BANKTRANLIST>
+                <LEDGERBAL>
+                    <BALAMT/>
+                    <DTASOF/>
+                </LEDGERBAL>
+            </STMTRS>
+        </STMTTRNRS>
+    </BANKMSGSRSV1>
+</OFX>
+"""
+
+
 def prettyPrint(xmlstr):
     dom = xml.dom.minidom.parseString(xmlstr)
-    print(dom.toprettyxml().replace("\t", "    "))
-
-def doctest_OfxWriter():
-    """Test OfxWriter functionality.
-
-    Create sample statement:
-
-        >>> statement = Statement("BID", "ACCID", "LTL")
-        >>> statement.lines.append(StatementLine(
-        ...     "1", datetime(2012, 2, 12), "Sample 1", 15.4))
-        >>> line = StatementLine("2", datetime(2012, 2, 12), "Sample 2", 25.0)
-        >>> line.payee = ''
-        >>> line.bank_account_to = BankAccount("SNORAS", "LT1232")
-        >>> line.bank_account_to.branch_id = "VNO"
-        >>> statement.lines.append(line)
-
-    Create writer:
-        >>> writer = ofx.OfxWriter(statement)
-
-    Set the generation time so it is always predictable
-        >>> writer.genTime = datetime(2012, 3, 3, 0, 0, 0)
-
-    Produce OFX output:
-        >>> prettyPrint(writer.toxml())
-        <?xml version="1.0" ?>
-        <!--
-        OFXHEADER:100
-        DATA:OFXSGML
-        VERSION:102
-        SECURITY:NONE
-        ENCODING:UTF-8
-        CHARSET:NONE
-        COMPRESSION:NONE
-        OLDFILEUID:NONE
-        NEWFILEUID:NONE
-        -->
-        <OFX>
-            <SIGNONMSGSRSV1>
-                <SONRS>
-                    <STATUS>
-                        <CODE>0</CODE>
-                        <SEVERITY>INFO</SEVERITY>
-                    </STATUS>
-                    <DTSERVER>20120303000000</DTSERVER>
-                    <LANGUAGE>ENG</LANGUAGE>
-                </SONRS>
-            </SIGNONMSGSRSV1>
-            <BANKMSGSRSV1>
-                <STMTTRNRS>
-                    <TRNUID>0</TRNUID>
-                    <STATUS>
-                        <CODE>0</CODE>
-                        <SEVERITY>INFO</SEVERITY>
-                    </STATUS>
-                    <STMTRS>
-                        <CURDEF>LTL</CURDEF>
-                        <BANKACCTFROM>
-                            <BANKID>BID</BANKID>
-                            <ACCTID>ACCID</ACCTID>
-                            <ACCTTYPE>CHECKING</ACCTTYPE>
-                        </BANKACCTFROM>
-                        <BANKTRANLIST>
-                            <DTSTART/>
-                            <DTEND/>
-                            <STMTTRN>
-                                <TRNTYPE>CHECK</TRNTYPE>
-                                <DTPOSTED>20120212</DTPOSTED>
-                                <TRNAMT>15.40</TRNAMT>
-                                <FITID>1</FITID>
-                                <MEMO>Sample 1</MEMO>
-                            </STMTTRN>
-                            <STMTTRN>
-                                <TRNTYPE>CHECK</TRNTYPE>
-                                <DTPOSTED>20120212</DTPOSTED>
-                                <TRNAMT>25.00</TRNAMT>
-                                <FITID>2</FITID>
-                                <MEMO>Sample 2</MEMO>
-                                <BANKACCTTO>
-                                    <BANKID>SNORAS</BANKID>
-                                    <BRANCHID>VNO</BRANCHID>
-                                    <ACCTID>LT1232</ACCTID>
-                                    <ACCTTYPE>CHECKING</ACCTTYPE>
-                                </BANKACCTTO>
-                             </STMTTRN>
-                        </BANKTRANLIST>
-                        <LEDGERBAL>
-                            <BALAMT/>
-                            <DTASOF/>
-                        </LEDGERBAL>
-                    </STMTRS>
-                </STMTTRNRS>
-            </BANKMSGSRSV1>
-        </OFX>
-        <BLANKLINE>
-    """
+    return dom.toprettyxml().replace("\t", "    ").replace('<!-- ', '<!--')
 
 
+class OfxWriterTest(TestCase):
+    def test_ofxWriter(self):
 
-def test_suite(*args):
-    return doctest.DocTestSuite(optionflags=(doctest.NORMALIZE_WHITESPACE|
-                                             doctest.ELLIPSIS|
-                                             doctest.REPORT_ONLY_FIRST_FAILURE|
-                                             doctest.REPORT_NDIFF
-                                             ))
-load_tests = test_suite
+        # Create sample statement:
+        statement = Statement("BID", "ACCID", "LTL")
+        statement.lines.append(StatementLine(
+            "1", datetime(2012, 2, 12), "Sample 1", 15.4))
+        line = StatementLine("2", datetime(2012, 2, 12), "Sample 2", 25.0)
+        line.payee = ''
+        line.bank_account_to = BankAccount("SNORAS", "LT1232")
+        line.bank_account_to.branch_id = "VNO"
+        statement.lines.append(line)
+
+        # Create writer:
+        writer = ofx.OfxWriter(statement)
+
+        # Set the generation time so it is always predictable
+        writer.genTime = datetime(2012, 3, 3, 0, 0, 0)
+
+        assert prettyPrint(writer.toxml()) == SIMPLE_OFX
--- src/ofxstatement/tests/test_parser.py
+++ src/ofxstatement/tests/test_parser.py
@@ -1,46 +1,34 @@
-import doctest
+import io
+from textwrap import dedent
+from unittest import TestCase
+from decimal import Decimal
 
 from ofxstatement.parser import CsvStatementParser
 
-def doctest_CsvStatementParser():
-    """Test generic CsvStatementParser
+class CsvStatementParserTest(TestCase):
 
-    Lets define some sample csv to parse and write it to file-like object
+    def test_simple_csv_parser(self):
+        # Test generic CsvStatementParser
 
-        >>> import io
-        >>> csv = '''
-        ... "2012-01-18","Microsoft","Windows XP",243.32,"1001"
-        ... "2012-02-14","Google","Adwords",23.54,"1002"
-        ... '''
-        >>> f = io.StringIO(csv)
+        # Lets define some sample csv to parse and write it to file-like object
+        csv = dedent('''
+            "2012-01-18","Microsoft","Windows XP",243.32,"1001"
+            "2012-02-14","Google","Adwords",23.54,"1002"
+            ''')
+        f = io.StringIO(csv)
 
-    Create and configure csv parser:
-        >>> parser = CsvStatementParser(f)
-        >>> parser.mappings = {"date": 0,
-        ...                    "payee": 1,
-        ...                    "memo": 2,
-        ...                    "amount": 3,
-        ...                    "id": 4}
+        # Create and configure csv parser:
+        parser = CsvStatementParser(f)
+        parser.mappings = {
+            "date": 0,
+            "payee": 1,
+            "memo": 2,
+            "amount": 3,
+            "id": 4
+        }
 
-    And parse csv:
-        >>> statement = parser.parse()
-        >>> len(statement.lines)
-        2
-        >>> statement.lines[0].amount
-        Decimal('243.32')
-        >>> statement.lines[1].payee
-        'Google'
-
-
-
-
-    """
-
-
-def test_suite(*args):
-    return doctest.DocTestSuite(optionflags=(doctest.NORMALIZE_WHITESPACE|
-                                             doctest.ELLIPSIS|
-                                             doctest.REPORT_ONLY_FIRST_FAILURE|
-                                             doctest.REPORT_NDIFF
-                                             ))
-load_tests = test_suite
+        # And parse csv:
+        statement = parser.parse()
+        self.assertEqual(len(statement.lines), 2)
+        self.assertEqual(statement.lines[0].amount, Decimal('243.32'))
+        self.assertEqual(statement.lines[1].payee, 'Google')
