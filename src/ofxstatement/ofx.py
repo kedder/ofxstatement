25d21d55f9b5e4598e26fc6d47e05bd8c0f65a89
Jake Cadwell <jaik03@hotmail.com>
2024-01-02 12:19:26 -0800

    fix formatting

--- src/ofxstatement/ofx.py
+++ src/ofxstatement/ofx.py
@@ -260,10 +260,11 @@ class OfxWriter(object):
             line.unit_price,
             precision=self.invest_transactions_float_precision,
         )
-        self.buildAmount("UNITS",
+        self.buildAmount(
+            "UNITS",
             line.units,
             precision=self.invest_transactions_float_precision,
-            )
+        )
 
         self.buildAmount(
             "TOTAL",
